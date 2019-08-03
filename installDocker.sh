4c1d830cb9764a6e1b37588dc112455e046564ea
baah-romero <46862934+baah-romero@users.noreply.github.com>
2019-08-03 18:57:57 +0200

    Instalación de docker
    Le falta realizar la descarga de las imagenes de los docker

---
+++ installDocker.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+DIRECT=/home
+DOT=/CEHv10
+DOCK=/docker
+DIR1=$DIRECT$DOT$DOCK
+DIR2=$DIRECT$DOT
+
+echo "Iniciando instalación docker..."
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+echo 'deb https://download.docker.com/linux/debian stretch stable' > /etc/apt/sources.list.d/docker.list
+echo "|-[++--------] 20% completado. Añadidos los certificados para Docker."
+apt-get update
+echo "|-[+++++++---] 30% completado. Actualizados los repositorios. Inicia instalación."
+apt-get install -y docker-ce
+echo "|-[+++++-----] 50% completado. Docker instalado."
+
+if [ -d "$DIR2" ] #Comprobar que existe la ruta /home/CEHv10
+ then
+	cd $DIR2
+	pwd	 
+else
+	cd $DIRECT
+	if [ -d "$DIRECT$DOT" ] #Comprobar que exista directorio /CEHv10 en /home
+		then
+			cd $DIR2 #Acceder si existe
+	else	
+		mkdir $DIR2 && cd $DIR2
+	fi
+	
+fi
+echo "|-[+++++++---] 65% completado. Creando ruta /home/CEHv10/docker"
+if [ -d "$DIR2$DOCK" ] #Comprobar /home/CEHv10/docker
+ then
+	cd $DIR1
+else
+	mkdir "$DIR2$DOCK"
+	cd $DIR1
+fi
+
+echo "|-[+++++++---] 75% completado. Ruta /home/CEHv10/docker CREADA!"
+echo "Se procede a crear los ficheros dvwaDocker.sh y wackoDocker.sh"
+
+	echo '#!/bin/bas' > $DIR1/dvwaDocker.sh 
+	echo 'docker run -p 127.0.0.1:9000:80 -it citizenstig/dvwa' >> $DIR1/dvwaDocker.sh 
+	echo '#!/bin/bas' > $DIR1/wackoDocker.sh 
+	echo 'docker run -p 127.0.0.1:8000:80 -it adamdoupe/wackopicko' >> $DIR1/wackoDocker.sh
+echo "|-[++++++++--] 80% completado. Creados los ficheros dvwaDocker.sh y wackoDocker.sh"
+	chmod +x $DIR1/dvwaDocker.sh && chmod +x $DIR1/wackoDocker.sh
+echo "|-[+++++++++-] 90% completado. Concedidos los permisos de ejecución para dvwaDocker.sh y wackoDocker.sh"
+	ln -s $DIR1/dvwaDocker.sh ~/dvwaDocker.sh
+	ln -s $DIR1/wackoDocker.sh ~/wackoDocker.sh 
+echo "|-[++++++++++] 100% completado."
+echo "Creados los enlaces para ejectar dvwaDocker.sh y wackoDocker.sh desde la raiz."
+cd ~
