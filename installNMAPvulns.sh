afbea888435b59b3e03a4aa9d8ea44c4c8520fd3
baah-romero <46862934+baah-romero@users.noreply.github.com>
2019-08-03 19:10:12 +0200

    Update installNMAPvulns.sh
    Corregidos los errores para ejecutar updateFiles.sh

--- installNMAPvulns.sh
+++ installNMAPvulns.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 RUTA='/usr/share/nmap/scripts/'
-cd /usr/share/nmap/scripts/
+cd /$RUTA
 git clone https://github.com/vulnersCom/nmap-vulners.git
 git clone https://github.com/scipag/vulscan.git
 ls $RUTA/vulscan/*.csv
 cd $RUTA/vulscan/utilities/updater
-chmod +x updateFiles.sh
-./updateFules.sh
+chmod +x $RUTA/vulscan/utilities/updater/updateFiles.sh
+./ $RUTA/vulscan/utilities/updater/updateFiles.sh
