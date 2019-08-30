05034c951b2a4a50b4b734738d29b34924873e10
baah-romero <46862934+baah-romero@users.noreply.github.com>
2019-08-30 12:52:50 -0400

    Delete DNS-PTRquery.py

--- pyhonTools/DNS-PTRquery.py
+++
@@ -1,8 +0,0 @@
-import dns.resolver
-ip=input('|-[+] Introducir IP: ')
-req='.'.join(reversed(ip.split(".")))+".in-addr.arpa"
-myResolver = dns.resolver.Resolver()
-myAnswer = myResolver.query(req, "PTR")
-for rdata in myAnswer:
-    print(f'\t|-[+] {rdata}')
-print('------\nFIN DE LA EJECUCIÓN\n-------')
