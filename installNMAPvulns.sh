d2cd152c2c54c448f3142d2a92f74562b32221c6
baah-romero <46862934+baah-romero@users.noreply.github.com>
2019-09-05 12:24:58 +0200

    solved problems with execution

--- installNMAPvulns.sh
+++ installNMAPvulns.sh
@@ -3,7 +3,7 @@ RUTA='/usr/share/nmap/scripts/'
 cd /$RUTA
 git clone https://github.com/vulnersCom/nmap-vulners.git
 git clone https://github.com/scipag/vulscan.git
-ls $RUTA/vulscan/*.csv
-cd $RUTA/vulscan/utilities/updater
-chmod +x $RUTA/vulscan/utilities/updater/updateFiles.sh
-./ $RUTA/vulscan/utilities/updater/updateFiles.sh
+ls $RUTAvulscan/*.csv
+cd $RUTAvulscan/utilities/updater
+chmod +x $RUTAvulscan/utilities/updater/updateFiles.sh
+./ $RUTAvulscan/utilities/updater/updateFiles.sh
